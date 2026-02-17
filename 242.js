9e1503728044020520b83ddb0ac3013a7f8672c0
abinjils7 <abinjills0904@gmail.com>
2026-02-17 13:14:32 +0530

    leetcode questions

---
+++ 242.js
@@ -0,0 +1,8 @@
+var isAnagram = function(s, t) {
+    const string=s.split(' ')
+    const text=t.split(' ')
+    console.log(string,text)
+
+};
+
+isAnagram('abin','bina')
---
+++ 349.js
@@ -0,0 +1,24 @@
+let nums1 = [1, 2, 3, 5];
+let nums2 = [1, 2, 2, 3, 4];
+
+var intersection = function (nums1, nums2) {
+  let map = new Map();
+
+  let comon = [];
+
+  for (let num of nums1) {
+    map.set(num, true);
+  }
+
+  for (let num of nums2) {
+    if (map.has(num)) {
+      comon.push(num);
+      map.delete(num);
+    }
+  }
+  console.log(comon);
+
+  return comon;
+};
+
+intersection(nums1, nums2);
